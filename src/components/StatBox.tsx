cc8abbe5eed54d45461b0b77c73ca6766c1aa42d
Cat Pereira <catherinelpa@gmail.com>
2025-04-22 17:20:24 -0700

    data type definition file progress

--- src/components/StatBox.tsx
+++ src/components/StatBox.tsx
@@ -1,14 +1,36 @@
-import { Stats } from "@/data/statdefinition";
+"use client"
+import { Stat, StatDefinition, Stats } from "@/data/statdefinition";
+import { useState } from "react";
 
 
 const StatBox: React.FC = () => {
-    const stats = Object.keys(Stats);
+    const [selectedStat, setSelectedStat] = useState<Stat | undefined>(undefined);
+    const stats = Object.values(Stat).map((stat) => [stat, Stats[stat]]) as [Stat, StatDefinition][];
+
+    const filteredStats = stats.filter((stat) => {
+        if (selectedStat === undefined) return true;
+        return selectedStat === stat[0]; // go by name instead
+    });
 
     return (
-        <div>
-            {stats.map((feature, _) => (
-                <p key={feature} className="text-gray-500">{feature}</p>
-            ))}
+        <div className="border border-gray-700 p-4 rounded-2xl bg-gray-900 shadow-lg">
+            {/* Title */}
+            <h2 className="text-lg font-semibold text-white">Stats</h2>
+
+
+            {/* Search Bar */}
+
+        
+            {/* Stats List */}
+            <div className="mt-2">
+                <div className="flex flex-wrap">
+                    {stats.map(([stat, def], _) => (
+                    <p key={stat} className="mr-2 mb-2 px-3 py-1 text-sm bg-cyan-500/15 text-cyan-300 rounded-md">
+                        {def.name}
+                    </p>
+                    ))}
+                </div>
+            </div> 
         </div>
     );
 };
--- src/data/statdefinition.ts
+++ src/data/statdefinition.ts
@@ -1,5 +1,34 @@
-export enum Stats {
+export enum Stat {
 	MedianHomePrice = "MedianHomePrice",
 	StatePopulation = "StatePopulation",
 	AveragePrecipitation = "AveragePrecipitation",
 }
+
+export enum DataType {
+	Numerical,
+	Categorical,
+}
+
+export interface StatDefinition {
+	name: string;
+	type: DataType;
+	numericalRange?: {
+		min: number;
+		max: number;
+	};
+}
+
+export const Stats: { [key in Stat]: StatDefinition } = {
+	[Stat.MedianHomePrice]: {
+		name: "Median Home Price",
+		type: DataType.Numerical,
+	},
+	[Stat.StatePopulation]: {
+		name: "State Population",
+		type: DataType.Numerical,
+	},
+	[Stat.AveragePrecipitation]: {
+		name: "Average Precipitation",
+		type: DataType.Numerical,
+	},
+};
